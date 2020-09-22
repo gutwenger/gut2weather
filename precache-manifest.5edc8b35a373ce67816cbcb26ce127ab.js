aa75064e8f83d5d88f9b530b668c0db45a975fc5
Fremont Cheng <fremont@Fremont.local>
2020-09-23 01:32:37 +0800

    Updates

---
+++ precache-manifest.5edc8b35a373ce67816cbcb26ce127ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94439639f56f48883c9ddd7c68b28d77",
+    "url": "/gut2weather/index.html"
+  },
+  {
+    "revision": "371adbc3a273f2cd6cd9",
+    "url": "/gut2weather/static/css/main.ec2f6a99.chunk.css"
+  },
+  {
+    "revision": "2a8bf1a29ebb0672e68e",
+    "url": "/gut2weather/static/js/2.141c5e27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gut2weather/static/js/2.141c5e27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "371adbc3a273f2cd6cd9",
+    "url": "/gut2weather/static/js/main.bb293a8e.chunk.js"
+  },
+  {
+    "revision": "36dde87e3d918a308ca6",
+    "url": "/gut2weather/static/js/runtime-main.13ef919c.js"
+  }
+]);
